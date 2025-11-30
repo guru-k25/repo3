f5bb2fb233c65ac0a33cff83e1ccef06a2969f8a
guru <guru.krishnakumar.123@gmail.com>
2025-11-30 13:28:24 +0530

    replaced the hardcoded server running on local with the dynamic one

--- server.js
+++ server.js
@@ -79,4 +79,5 @@ app.post('/api/verify-payment', async (req, res) => {
     res.json({ success: true });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
